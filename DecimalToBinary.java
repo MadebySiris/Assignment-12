fdd8a758c0278fe0dea3350c097d8989c0021651
Siris St Victor <144554416+MadebySiris@users.noreply.github.com>
2024-12-01 23:05:43 -0500

    Update DecimalToBinary.java

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,37 +1,37 @@
 public class DecimalToBinary {
-    // Recursive method to convert decimal to binary
+    
     public static String decimalToBinaryRecursive(int n) {
-        // Base case: if number is 0 or 1, return as string
+       
         if (n <= 1) {
             return String.valueOf(n);
         }
         
-        // Recursive case: divide by 2 and add remainder
+       
         return decimalToBinaryRecursive(n / 2) + (n % 2);
     }
 
-    // Iterative method to convert decimal to binary
+   
     public static String decimalToBinaryIterative(int n) {
-        // Special case for 0
+       
         if (n == 0) {
             return "0";
         }
         
-        // Use StringBuilder to build binary number
+       
         StringBuilder binary = new StringBuilder();
         
-        // Continue until number becomes 0
+    
         while (n > 0) {
-            // Add remainder (0 or 1) to the front of the string
+            
             binary.insert(0, n % 2);
-            // Divide number by 2
+            
             n = n / 2;
         }
         
         return binary.toString();
     }
 
-    // Main method to test and show results
+    
     public static void main(String[] args) {
         int[] testNumbers = {0, 5, 10, 15, 42, 100};
         
